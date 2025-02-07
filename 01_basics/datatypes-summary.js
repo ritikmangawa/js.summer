c8e452b7defd3a17caddc591d51d79d114ed1b03
ritikmangawa <160820613+ritikmangawa@users.noreply.github.com>
2025-02-07 18:47:27 +0000

    datatypessummary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,29 +1,62 @@
-"use strict"; // treat all JS code as newer version
+//  Primitive
 
-// alert( 3 + 3) // we are using nodejs, not browser
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
 
-console.log(3 
-    +
-     3) // code readability should be high
+const score = 100
+const scoreValue = 100.3
 
-console.log("Hitesh")
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
 
+const id = Symbol('123')
+const anotherId = Symbol('123')
 
-let name = "hitesh"
-let age = 18
-let isLoggedIn = false
-let state;
+console.log(id === anotherId);
 
-// number => 2 to power 53
-// bigint
-// string => ""
-// boolean => true/false
-// null => standalone value
-// undefined => 
-// symbol => unique
+// const bigNumber = 3456543576654356754n
 
 
-// object
 
-console.log(typeof undefined); // undefined
-console.log(typeof null); // object
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++
+//stack (primitive) u will get copy of declared variable ,   Heap(non primitive)  here u will get reference , meanse if u do some change then change happen in orginal memory
+
+
+let myYoutube =  "hiteshchoudarydotcom"
+
+let anothername = myYoutube
+anothername  = "Chaiaurcode"
+
+console.log(myYoutube);   //"hiteshchoudarydotcom"
+console.log(anothername);    //"Chaiaurcode"
+
+let userOne = {
+    email:  "user@googel.com",
+    upi:    " user@ybl"
+
+}
+let userTwo   = userOne
+userTwo.email = "rkchoudhary0812@gmail.com"
+
+console.log(userOne.email);    //rkchoudhary0812@gmail.com
+console.log(userTwo.email);    //rkchoudhary0812@gmail.com
