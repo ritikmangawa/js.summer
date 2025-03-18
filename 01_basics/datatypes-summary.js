04c33a4ef0eae632134276fe0ccd3138934a1d4b
RITIK MANGAWA <160820613+ritikmangawa@users.noreply.github.com>
2025-03-18 12:48:51 +0530

    Update datatypes-summary.js

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,6 +1,7 @@
 //  Primitive
 
 //  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+// these are call by value
 
 const score = 100
 const scoreValue = 100.3
@@ -12,7 +13,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+console.log(id === anotherId);             //false
 
 // const bigNumber = 3456543576654356754n
 
@@ -26,17 +27,31 @@ const heros = ["shaktiman", "naagraj", "doga"];
 let myObj = {
     name: "hitesh",
     age: 22,
-}
+}// datatype is object
+
 
 const myFunction = function(){
     console.log("Hello world");
-}
+}// data type is function
 
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
 
+// Return type of variables in JavaScript
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
 
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
 
 //+++++++++++++++++++++++++++++++++++++++++++++++++++
 //stack (primitive) u will get copy of declared variable ,   Heap(non primitive)  here u will get reference , meanse if u do some change then change happen in orginal memory
