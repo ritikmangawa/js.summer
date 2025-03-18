4a7093a428e4ee4372a7067f8e1ab32cae4b5c1c
RITIK MANGAWA <160820613+ritikmangawa@users.noreply.github.com>
2025-03-18 18:23:42 +0530

    Update 05_string.js

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -1,7 +1,7 @@
 const name = "hitesh"
 const repoCount = 50
 
-// console.log(name + repoCount + " Value");
+// console.log(name + repoCount + " Value");             //hitesh50 Value
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
