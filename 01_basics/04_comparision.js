94c3d6d6c220215a212207f3b629fc701cd2efba
RITIK MANGAWA <160820613+ritikmangawa@users.noreply.github.com>
2025-03-18 08:22:13 +0530

    Update 04_comparision.js

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -8,13 +8,17 @@
 // console.log("2" > 1);
 // console.log("02" > 1);
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+//the reason is that an equaility check == and comparisons   <> >= <= work differently.
+//comparison convert null to number . treating it as 0.
+//that's why (3)null >=0 is true and (1) null > 0 is false.
 
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
+console.log(null > 0);        //false
+console.log(null == 0);       //false
+console.log(null >= 0);       //true
+
+console.log(undefined == 0);  //false
+console.log(undefined > 0);   //false
+console.log(undefined < 0);   //false
 
 // === 
 
