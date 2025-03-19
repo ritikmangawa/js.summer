43a09c7ba3cb7ae78e378232b390b310ab5c0baf
RITIK MANGAWA <160820613+ritikmangawa@users.noreply.github.com>
2025-03-19 23:00:49 +0530

    Update 01_array.js

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -4,19 +4,19 @@ const myArr = [0, 1, 2, 3, 4, 5]   // 0 1 2 etc are called elements
 const myHeors = ["shaktiman", "naagraj"]
 
 const myArr2 = new Array(1, 2, 3, 4)
-// console.log(myArr[1]);
+// console.log(myArr[1]);        //0
 
 // Array methods
 
 // myArr.push(6)
 // myArr.push(7)
-// myArr.pop()
+// myArr.pop()       //remove last value
 
-// myArr.unshift(9)
-// myArr.shift()
+// myArr.unshift(9)              //insert 9 at start
+// myArr.shift()                   //remove first element
 
-// console.log(myArr.includes(9));
-// console.log(myArr.indexOf(3));
+// console.log(myArr.includes(9));      //false
+// console.log(myArr.indexOf(3));      //
 
 // const newArr = myArr.join()
 
@@ -36,7 +36,7 @@ console.log("B ", myArr);
 
 const myn2 = myArr.splice(1, 3)
 console.log("C ", myArr);
-console.log(myn2);
+console.log(myn2); 
 
 
 // 1. Array is written in brackets [  ].
