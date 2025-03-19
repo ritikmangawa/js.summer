ce3f700c0ad897b74aefdea291261bf5f12bb5e3
RITIK MANGAWA <160820613+ritikmangawa@users.noreply.github.com>
2025-03-19 10:13:55 +0530

    Update 05_string.js

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -3,12 +3,12 @@ const repoCount = 50
 
 // console.log(name + repoCount + " Value");             //hitesh50 Value
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);  //string interpolation
 
 const gameName = new String('hitesh-hc-com')
 
 // console.log(gameName[0]);
-// console.log(gameName.__proto__);
+// console.log(gameName.__proto__);   //{}
 
 
 // console.log(gameName.length);
@@ -17,14 +17,14 @@ console.log(gameName.charAt(2));
 console.log(gameName.indexOf('t'));
 
 const newString = gameName.substring(0, 4)
-console.log(newString);
+console.log(newString);     //hite
 
 const anotherString = gameName.slice(-8, 4)
-console.log(anotherString);
+console.log(anotherString);          //for hitesh-hc        ite
 
 const newStringOne = "   hitesh    "
 console.log(newStringOne);
-console.log(newStringOne.trim());
+console.log(newStringOne.trim());         //remove space
 
 const url = "https://hitesh.com/hitesh%20choudhary"
 
